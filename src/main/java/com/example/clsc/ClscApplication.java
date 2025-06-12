9230d8ac5b9628be5a5eefa51d6f0859733ef3b0
harshit <harshit.shambharkar@recykal.com>
2025-06-12 16:17:00 +0530

    initialising repo

---
+++ src/main/java/com/example/clsc/ClscApplication.java
@@ -0,0 +1,13 @@
+package com.example.clsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClscApplication.class, args);
+	}
+
+}
