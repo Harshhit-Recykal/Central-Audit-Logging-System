0672f0a79b176a2bf9bdb499fba6196186d9ec09
harshit <harshit.shambharkar@recykal.com>
2025-06-12 16:47:29 +0530

    adding audit entity

---
+++ src/main/java/com/example/clsc/entity/AuditLog.java
@@ -0,0 +1,48 @@
+package com.example.clsc.entity;
+
+import com.example.clsc.enums.ActionType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "audit_log")
+public class AuditLog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "entity_name")
+    private String entityName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action")
+    private ActionType action;
+
+    @Column(name = "changed_by")
+    private String changedBy;
+
+    @Column(name = "changed_at")
+    private LocalDateTime changedAt;
+
+    @Column(name = "request_id")
+    private String requestId;
+
+    @Lob
+    @Column(name = "field_changes")
+    private String fieldChanges;
+
+    @Lob
+    @Column(name = "raw_data_before")
+    private String rawDataBefore;
+
+    @Lob
+    @Column(name = "raw_data_after")
+    private String rawDataAfter;
+
+}
---
+++ src/main/java/com/example/clsc/enums/ActionType.java
@@ -0,0 +1,7 @@
+package com.example.clsc.enums;
+
+public enum ActionType {
+    CREATE,
+    UPDATE,
+    DELETE
+}
