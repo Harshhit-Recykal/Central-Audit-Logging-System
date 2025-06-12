5f3b6b49d988d6257b180913214c9356ecd6eb6a
harshkhandelwal97-admin <harsh.khandelwal@recykal.com>
2025-06-12 17:20:47 +0530

    added appprop

--- src/main/java/com/example/clsc/service/LogConsumer.java
+++ src/main/java/com/example/clsc/service/LogConsumer.java
@@ -2,6 +2,8 @@ package com.example.clsc.service;
 
 import com.example.clsc.config.RabbitMQConfig;
 import com.example.clsc.constants.ConfigConstants;
+import com.example.clsc.dto.AuditEvent;
+import com.example.clsc.entity.AuditLog;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.core.log.LogMessage;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class LogConsumer {
 
     @RabbitListener(queues = ConfigConstants.QUEUE_NAME)
-    public void receiveLog(LogMessage message) {
+    public void receiveLog(AuditEvent message) {
         System.out.println("Received Log: " + message);
     }
 }
