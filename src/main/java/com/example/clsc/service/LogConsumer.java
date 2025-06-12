12c23f20f1d5c5b00ad6a06c6ac111ee0c013f63
harshit <harshit.shambharkar@recykal.com>
2025-06-12 17:36:28 +0530

    correcting rabbit mq config

--- src/main/java/com/example/clsc/service/LogConsumer.java
+++ src/main/java/com/example/clsc/service/LogConsumer.java
@@ -1,11 +1,8 @@
 package com.example.clsc.service;
 
-import com.example.clsc.config.RabbitMQConfig;
 import com.example.clsc.constants.ConfigConstants;
 import com.example.clsc.dto.AuditEvent;
-import com.example.clsc.entity.AuditLog;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.core.log.LogMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +10,6 @@ public class LogConsumer {
 
     @RabbitListener(queues = ConfigConstants.QUEUE_NAME)
     public void receiveLog(AuditEvent message) {
-        System.out.println("Received Log: " + message);
+        System.out.println("Received Log: " + message.toString());
     }
 }
