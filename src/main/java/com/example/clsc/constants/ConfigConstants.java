138b76db849c203230f5befcd82d3af95c9e90cf
harshit <harshit.shambharkar@recykal.com>
2025-06-16 12:12:52 +0530

    config constant rename

--- src/main/java/com/example/clsc/constants/ConfigConstants.java
+++ src/main/java/com/example/clsc/constants/ConfigConstants.java
@@ -1,7 +1,9 @@
 package com.example.clsc.constants;
 
 public final class ConfigConstants {
-    public static final String QUEUE_NAME = "ecommerce_queue";
-    public static final String TOPIC_NAME = "ecommerce_topic";
-    public static final String ROUTING_KEY = "ecommerce_route";
+    public static final String QUEUE_NAME = "audit.log.queue";
+
+    public static final String TOPIC_NAME = "audit.log.topic";
+
+    public static final String ROUTING_KEY = "audit.log.route.key";
 }
