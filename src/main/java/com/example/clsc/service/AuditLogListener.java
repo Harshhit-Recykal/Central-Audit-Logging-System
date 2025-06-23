a6d433f834d57c8da9600b7bf9d7502e1935e214
harshkhandelwal97-admin <harsh.khandelwal@recykal.com>
2025-06-23 15:49:21 +0530

    added missing log logic

--- src/main/java/com/example/clsc/service/AuditLogListener.java
+++ src/main/java/com/example/clsc/service/AuditLogListener.java
@@ -1,3 +1,4 @@
+//
 //package com.example.clsc.service;
 //
 //import com.example.clsc.constants.ConfigConstants;
@@ -11,7 +12,7 @@
 //import org.springframework.amqp.rabbit.annotation.RabbitListener;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.stereotype.Service;
-//import java.sql.Timestamp;
+//
 //import java.util.HashMap;
 //import java.util.Map;
 //import java.util.Optional;
@@ -43,8 +44,7 @@
 //        auditLog.setChangedAt(message.getTimestamp());
 //        auditLog.setChangedBy(message.getChangedBy());
 //        auditLog.setRequestId(message.getRequestId());
-//        auditLog.setRawDataAfter(message.getRawDataAfter().toString());
-//        auditLog.setRawDataBefore(message.getRawDataBefore().toString());
+//        //  auditLog.setRawDataAfter(message.getRawDataAfter().toString());
 //
 //        try {
 //            String rawAfterJson = message.getRawDataAfter() != null
@@ -52,101 +52,64 @@
 //                    : null;
 //            auditLog.setRawDataAfter(rawAfterJson);
 //
+//            // Load previous version and calculate diffs
 //            if (rawAfterJson != null) {
 //                Optional<AuditLog> previousLogOpt = auditLogRepo
 //                        .findTopByEntityNameAndEntityIdOrderByChangedAtDesc(
 //                                message.getEntityName(), message.getEntityId()
 //                        );
-//            // Load previous version and calculate diffs
 //
-//            if (previousLogOpt.isPresent()) {
-//                AuditLog previousLog = previousLogOpt.get();
-//
-//                Timestamp previous = previousLog.getRawDataAfter().get("createdAt");
-//                Timestamp current =  objectMapper.writeValueAsTimestamp(message.getRawDataBefore().get("timestamp"));
-//
-//                if( previous == current  || previous.after(current)){
+//                if (previousLogOpt.isPresent()) {
+//                    AuditLog previousLog = previousLogOpt.get();
 //                    auditLog.setRawDataBefore(previousLog.getRawDataAfter());
-//                    Map<String ,Object> currentData = (Map<String ,Object>)message.getRawDataAfter();
+//                    Map<String, Object> currentData = (Map<String, Object>) message.getRawDataAfter();
 //                    Map<String, Object> previousData = objectMapper.readValue(
-//                            previousLog.getRawDataAfter(), new TypeReference<>() {}
+//                            previousLog.getRawDataAfter(), new TypeReference<>() {
+//                            }
 //                    );
-//                    Map<String, Object[]> fieldChanges =  findDiffs( currentData , previousData);
-//                    auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
-//                }
-//                else{
-//                    // logic for handling the missing log
-//                    AuditEvent missingLog = new AuditEvent();
-//                    missingLog.setEntityName(previousLog.getEntityName());
-//                    missingLog.setEntityId(previousLog.getEntityId());
-//                    missingLog.setAction("UPDATE");
-//                    missingLog.setTimestamp(message.getRawDataBefore().get("updatedAt"));
-//                    missingLog.setChangedBy(previousLog.getChangedBy());
-//                    missingLog.setRequestId(previousLog.getRequestId()); /// we need to set it to random
-//                    missingLog.setRawDataBefore(previousLog.getRawDataAfter());
-//                    missingLog.setRawDataAfter(message.getRawDataBefore());
-//                    Map<String ,Object> currentData = (Map<String ,Object>)message.getRawDataBefore();
-//                    Map<String, Object> previousData = objectMapper.readValue(
-//                            previousLog.getRawDataAfter(), new TypeReference<>() {}
-//                    );
-//                    Map<String, Object[]> fieldChanges =  findDiffs( currentData , previousData);
-//                    auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
-//                    auditLogRepo.save(missingLog);
+//                    Map<String, Object[]> fieldChanges = findDiffs(currentData, previousData);
 //
-//                    Map<String ,Object> currentVal = (Map<String ,Object>)message.getRawDataAfter();
-//                    Map<String, Object> previousVal = (Map<String, Object>)message.getRawDataAfter();
-//                    Map<String, Object[]> fieldChange =  findDiffs( currentVal , previousVal);
-//                    auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChange));
-//
+//                    auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
 //                }
 //            }
+//        } catch (Exception e) {
+//            auditLog.setRawDataBefore("ERROR_PROCESSING_DATA: " + e.getMessage());
+//            auditLog.setRawDataAfter("ERROR_PROCESSING_DATA: " + e.getMessage());
 //        }
-//    }
-//    catch (Exception e) {
-//        auditLog.setRawDataBefore("ERROR_PROCESSING_DATA: " + e.getMessage());
-//        auditLog.setRawDataAfter("ERROR_PROCESSING_DATA: " + e.getMessage());
-//    }
-//    auditLogRepo.save(auditLog);
+//        auditLogRepo.save(auditLog);
 //    }
 //}
-package com.example.clsc.service;
 
+package com.example.clsc.service;
 import com.example.clsc.constants.ConfigConstants;
 import com.example.clsc.dto.AuditEvent;
 import com.example.clsc.entity.AuditLog;
 import com.example.clsc.enums.ActionType;
 import com.example.clsc.repository.AuditLogRepo;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
-
+import java.util.UUID;
 import static com.example.clsc.utils.CalculateJsonDiffs.findDiffs;
 
 @Service
 @SuppressWarnings("unchecked")
 public class AuditLogListener {
-
     private final AuditLogRepo auditLogRepo;
-
     private final ObjectMapper objectMapper;
-
     @Autowired
     public AuditLogListener(AuditLogRepo auditLogRepo) {
         this.auditLogRepo = auditLogRepo;
         this.objectMapper = new ObjectMapper();
     }
-
     @RabbitListener(queues = ConfigConstants.QUEUE_NAME)
     public void receiveLog(AuditEvent message) {
         System.out.println("Received Log: " + message);
-
         AuditLog auditLog = new AuditLog();
         auditLog.setEntityName(message.getEntityName());
         auditLog.setEntityId(message.getEntityId());
@@ -154,38 +117,78 @@ public class AuditLogListener {
         auditLog.setChangedAt(message.getTimestamp());
         auditLog.setChangedBy(message.getChangedBy());
         auditLog.setRequestId(message.getRequestId());
-        //  auditLog.setRawDataAfter(message.getRawDataAfter().toString());
 
         try {
-            String rawAfterJson = message.getRawDataAfter() != null
-                    ? objectMapper.writeValueAsString(message.getRawDataAfter())
-                    : null;
+            String rawAfterJson = objectMapper.writeValueAsString(message.getRawDataAfter());
             auditLog.setRawDataAfter(rawAfterJson);
 
-            // Load previous version and calculate diffs
-            if (rawAfterJson != null) {
-                Optional<AuditLog> previousLogOpt = auditLogRepo
-                        .findTopByEntityNameAndEntityIdOrderByChangedAtDesc(
-                                message.getEntityName(), message.getEntityId()
-                        );
+            Optional<AuditLog> previousLogOpt = auditLogRepo
+                    .findTopByEntityNameAndEntityIdOrderByChangedAtDesc(
+                            message.getEntityName(), message.getEntityId());
 
-                if (previousLogOpt.isPresent()) {
-                    AuditLog previousLog = previousLogOpt.get();
-                    auditLog.setRawDataBefore(previousLog.getRawDataAfter());
-                    Map<String ,Object> currentData = (Map<String ,Object>)message.getRawDataAfter();
-                    Map<String, Object> previousData = objectMapper.readValue(
-                            previousLog.getRawDataAfter(), new TypeReference<>() {}
-                    );
-                    Map<String, Object[]> fieldChanges =  findDiffs( currentData , previousData);
+            if (previousLogOpt.isPresent()) {
+                AuditLog previousLog = previousLogOpt.get();
+                auditLog.setRawDataBefore(previousLog.getRawDataAfter());
 
-                    auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
+                LocalDateTime previousUpdatedAtAfter = extractUpdatedAt(previousLog.getRawDataAfter());
+                LocalDateTime currentUpdatedAtBefore = extractUpdatedAt(objectMapper.writeValueAsString(message.getRawDataBefore()));
+
+                if (previousUpdatedAtAfter!= null && currentUpdatedAtBefore != null) {
+                    if (previousUpdatedAtAfter == currentUpdatedAtBefore) {
+//                 Normal diff case
+                auditLog.setRawDataBefore(previousLog.getRawDataAfter());
+
+                Map<String, Object> currentData = (Map<String, Object>) message.getRawDataAfter();
+                Map<String, Object> previousData = objectMapper.readValue(
+                        previousLog.getRawDataAfter(), new TypeReference<>() {
+                        });
+                Map<String, Object[]> fieldChanges = findDiffs(currentData, previousData);
+
+                auditLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
+
+                    }
+            else {
+                        // in every case misslog is there
+                        AuditLog missingLog = createMissingAuditLog(message, previousLog);
+                        auditLogRepo.save(missingLog);
+                    }
                 }
             }
+
+        } catch (Exception e) {
+            String error = "ERROR_PROCESSING_DATA: " + e.getMessage();
+            auditLog.setRawDataBefore(error);
+            auditLog.setRawDataAfter(error);
         }
-        catch (Exception e) {
-            auditLog.setRawDataBefore("ERROR_PROCESSING_DATA: " + e.getMessage());
-            auditLog.setRawDataAfter("ERROR_PROCESSING_DATA: " + e.getMessage());
-        }
+
         auditLogRepo.save(auditLog);
+    }
+
+    private LocalDateTime extractUpdatedAt(String json) {
+        try {
+            Map<String, Object> map = objectMapper.readValue(json, new TypeReference<>() {});
+            Object updatedAt = map.get("updatedAt");
+            return (updatedAt instanceof LocalDateTime) ? (LocalDateTime) updatedAt : null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private AuditLog createMissingAuditLog(AuditEvent message, AuditLog previousLog) throws Exception {
+        AuditLog missingLog = new AuditLog();
+        missingLog.setEntityName(previousLog.getEntityName());
+        missingLog.setEntityId(previousLog.getEntityId());
+        missingLog.setAction(ActionType.UPDATE);
+        missingLog.setChangedAt(previousLog.getChangedAt());
+        missingLog.setChangedBy(previousLog.getChangedBy());
+        missingLog.setRequestId(UUID.randomUUID().toString());
+        missingLog.setRawDataBefore(previousLog.getRawDataAfter());
+        String rawBeforeJson = objectMapper.writeValueAsString(message.getRawDataBefore());
+        missingLog.setRawDataAfter(rawBeforeJson);
+        Map<String, Object> currentData = (Map<String, Object>) message.getRawDataBefore();
+        Map<String, Object> previousData = objectMapper.readValue(
+                previousLog.getRawDataAfter(), new TypeReference<>() {});
+        Map<String, Object[]> fieldChanges = findDiffs(currentData, previousData);
+        missingLog.setFieldChanges(objectMapper.writeValueAsString(fieldChanges));
+        return missingLog;
     }
 }
